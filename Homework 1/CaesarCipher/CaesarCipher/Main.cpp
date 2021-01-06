35c9918ddfd226a54cacc2f42b08de16a6cc9616
SurveySays <brennen.boese@oit.edu>
2021-01-05 18:49:57 -0800

    started welcome

--- Homework 1/CaesarCipher/CaesarCipher/Main.cpp
+++ Homework 1/CaesarCipher/CaesarCipher/Main.cpp
@@ -1,7 +1,59 @@
 #include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+#include "Caesar.h"
+
+void Welcome();
+void ErrorMessage();
+char cipherChoice();
+
+//TODO: Implement Ceasar class
 
 int main() {
 
+	Welcome();
+
+	if (cipherChoice() == 'E') {
+		cout << "encrypt called";
+	} 
+	else {
+		cout << "decrypt called";
+	}
+	
 
 	return 0;
 }
+
+void Welcome() {
+
+	cout << "**********************************" << endl;
+	cout << "WELCOME to Caesar Cipher Program" << endl;
+	cout << "**********************************" << endl;
+}
+
+void ErrorMessage() {
+
+	cout << endl << "Please choose the correct option" << endl;
+}
+
+char cipherChoice() {
+
+	char choice{ 0 };
+
+	cout << "Please enter 'e' for Encryption or 'd' for Decryption: ";
+	cin >> choice;
+
+	if (choice == 'E' || choice == 'e') {
+		return 'E';
+	}
+	else if (choice == 'D' || choice == 'd') {
+		return 'D';
+	}
+	else {
+		ErrorMessage();
+		cipherChoice();
+	}
+}
+
